06188f362fc24dadc502bfd9d8174e72b3c792a6
DNAGen <chuthienduc16062003@gmail.com>
2025-02-19 23:11:46 +0700

    Add_ColorRepo

--- datn/src/main/java/backend/datn/repositories/ColorRepository.java
+++ datn/src/main/java/backend/datn/repositories/ColorRepository.java
@@ -1,9 +1,14 @@
 package backend.datn.repositories;
 
 import backend.datn.entities.Color;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-@Repository
 public interface ColorRepository extends JpaRepository<Color, Integer> {
+    @Query("SELECT c FROM Color c WHERE :search IS NULL OR  LOWER(c.colorName) LIKE LOWER(CONCAT('%', :search, '%'))")
+    Page<Color> searchColors(@Param("search") String search, Pageable pageable);
+    boolean existsByColorName(String name);
 }
