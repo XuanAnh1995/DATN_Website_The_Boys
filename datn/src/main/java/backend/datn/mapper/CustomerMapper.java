9de89913d6ddcffd394eb8f34d67f829552c1348
ThaisonTran21 <tranthaison21062001@gmail.com>
2025-02-22 21:09:45 +0700

    bo sung add Customer
    mapper

--- datn/src/main/java/backend/datn/mapper/CustomerMapper.java
+++ datn/src/main/java/backend/datn/mapper/CustomerMapper.java
@@ -1,15 +1,22 @@
 package backend.datn.mapper;
 
-import backend.datn.dto.response.CustomerRespone;
+import backend.datn.dto.response.CustomerResponse;
 import backend.datn.entities.Customer;
 
 
 public class CustomerMapper {
-    public static CustomerRespone toCustomerRespone(Customer customer) {
-        return CustomerRespone.builder().
-                id(customer.getId()).
-                fullname(customer.getFullname()).
-                username(customer.getUsername()).
-                build();
+    public static CustomerResponse toCustomerResponse(Customer customer) {
+        return CustomerResponse.builder()
+                .id(customer.getId())
+                .customerCode(customer.getCustomerCode())
+                .fullname(customer.getFullname())
+                .username(customer.getUsername())
+                .email(customer.getEmail())
+                .phone(customer.getPhone())
+                .createDate(customer.getCreateDate())
+                .updateDate(customer.getUpdateDate())
+                .forgetPassword(customer.getForgetPassword())
+                .status(customer.getStatus())
+                .build();
     }
 }
